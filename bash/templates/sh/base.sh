908366898b3fa74586b335a0640f7c7f59e27cfb
wjorden <jorden.williams.1989@gmail.com>
2024-07-02 18:36:23 -0400

    adding bash generation support

---
+++ bash/templates/sh/base.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#################################################
+# 	The whole point of this is to make 	#
+#     making bash scripts faster and easier.	#
+#################################################
+
+# DEBUG #
+# set -x
+
+# check run as root (sudo/doas)
+# if [[ `id -u` -ne 0 ]]; then
+#	printf "Please run as root."
+#	exit
+# fi
+
+
